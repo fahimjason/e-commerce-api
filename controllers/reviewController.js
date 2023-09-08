3f05cd38346a1c92b5c8e045e48b0f428c813ea4
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-09-08 19:45:22 +0600

    Add review routes structure

---
+++ controllers/reviewController.js
@@ -0,0 +1,32 @@
+const createReview = async (req, res) => {
+    res.send('Create review');
+};
+
+const getAllReviews = async (req, res) => {
+    res.send('Get all reviews');
+};
+
+const getSingleReview = async (req, res) => {
+    res.send('Get single review');
+};
+
+const updateReview = async (req, res) => {
+    res.send('Update review');
+};
+
+const deleteReview = async (req, res) => {
+    res.send('Delete review');
+};
+
+const getSingleProductReviews = async (req, res) => {
+    res.send('Get single product reviews');
+};
+
+module.exports = {
+    createReview,
+    getAllReviews,
+    getSingleReview,
+    updateReview,
+    deleteReview,
+    getSingleProductReviews,
+};
